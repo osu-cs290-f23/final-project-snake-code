2a3c83519ef3487774e6f21168bbe5056bcca856
sydneyear <sydneycear@gmail.com>
2023-12-09 22:50:17 -0800

    updated leaderboard

--- snakeServer.js
+++ snakeServer.js
@@ -9,17 +9,25 @@ var port = process.env.PORT || 3000
 app.engine("handlebars", exphbs.engine({ defaultLayout : 'main' }))
 app.set("view engine","handlebars")
 
+var leaderboardData = require("./leaderboardData.json")
  
 app.use(express.static('static'))
 
-app.get('/', function (req, res){
-    res.status(200).render("gameView",{
-
+app.get('/leaderboard', function (req, res){
+    console.log("leaderboard")
+    res.status(200).render("leaderboard",{
+        leaderboardData: leaderboardData
     })
-    console.log("main page")
 })
 
-app.get('*', function (req, res) {
+app.get('/', function (req, res){
+    res.status(200).render("gameView",{
+        
+    })
+    console.log("game page")
+})
+
+app.get('*', function (req, res) {//broken - always runs after game page and leaderboard requests run
     res.status(404).render("404",{
         
     })
